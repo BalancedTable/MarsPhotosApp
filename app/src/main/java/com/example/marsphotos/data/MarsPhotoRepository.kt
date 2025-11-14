82ad3d1a6f48b365f2688623331d5c4ff2fe6d96
Dmitriy <dimkam2004@gmail.com>
2025-11-14 17:03:43 +0200

    First commit

---
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotoRepository.kt
@@ -0,0 +1,14 @@
+package com.example.marsphotos.data
+
+import com.example.marsphotos.network.MarsApiService
+import com.example.marsphotos.network.MarsPhoto
+
+interface MarsPhotoRepository {
+    suspend fun getMarsPhotos(): List<MarsPhoto>
+}
+
+class NetworkMarsPhotoRepository(
+    private val marsApiService: MarsApiService
+): MarsPhotoRepository {
+    override suspend fun getMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
+}
